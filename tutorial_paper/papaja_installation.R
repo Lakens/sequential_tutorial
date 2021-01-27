4a1b89c88f1fd006e6552ea8289e2d79efcf46cd
Lakens <D.Lakens@tue.nl>
2021-01-27 08:02:13 +0100

    preprint

---
+++ tutorial_paper/papaja_installation.R
@@ -0,0 +1,10 @@
+
+# papaja installation
+devtools::install_github("crsh/papaja")
+
+# Downloading GitHub repo crsh/papaja@HEAD
+# These packages have more recent versions available.
+# It is recommended to update all of them.
+# Which would you like to update?
+# Choose 1: All
+install.packages("backports")
